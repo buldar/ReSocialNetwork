b65d20d6995e857559384a3791c1907090e691d9
Buldar <buldar666@gmail.com>
2022-07-04 20:01:35 +0300

    changed Users.Container.js container component and Users.jsx presentation component

--- src/components/Users/Users.Container.js
+++ src/components/Users/Users.Container.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import {Users} from "./Users";
 import {connect} from "react-redux";
 import {
     followAC,
@@ -7,10 +6,40 @@ import {
     setTotalUsersCountAC,
     setUserAC,
     unfollowAC,
-    usersReducer
 } from "../../redux/users-reducer";
-import Users from "./Users";
+import axios from "axios";
+import {Users} from "./Users";
 
+class UsersAPIComponent extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (page) => {
+        this.props.setCurrentPage(page)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return <Users
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            unfollow={this.props.unfollow}
+            follow={this.props.follow}
+        />
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -20,6 +49,7 @@ const mapStateToProps = (state) => {
         currentPage:state.usersPage.currentPage
     }
 }
+
 const mapDispatchToProps = (dispatch) => {
     return {
         follow: (userId)=>{
@@ -41,4 +71,4 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent)
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,56 +1,34 @@
 import React from "react";
-import style from './Users.module.css'
-import axios from "axios";
-import noAva from '../../pic/noname.jpg'
+import style from "./Users.module.css";
+import noAva from "../../pic/noname.jpg";
 
+export const Users = (props) => {
 
-class Users extends React.Component {
-
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
-            })
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
+    }
+    let newPages
+    if (props.currentPage <= 5) {
+        newPages = pages.slice(0, 10)
+    } else {
+        newPages = pages.slice(props.currentPage - 5, props.currentPage + 5)
     }
 
-    onPageChanged = (page) => {
-        this.props.setCurrentPage(page)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-            })
-    }
-
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-        let pages = []
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-        let newPages
-        if (this.props.currentPage<=5) {
-            newPages = pages.slice(0,10)
-        } else {
-            newPages=pages.slice(this.props.currentPage-5,this.props.currentPage+5)
-        }
-
-
-
-
-        return <div>
+    return (
+        <div>
             <div>
                 {
                     newPages.map(p => {
-                        return <span className={p === this.props.currentPage && style.selectedPage}
-                                     onClick={()=>{this.onPageChanged(p)}}>{p} </span>
+                        return <span className={p === props.currentPage && style.selectedPage}
+                                     onClick={()=>{props.onPageChanged(p)}}>{p} </span>
                     })
                 }
             </div>
 
             {
-                this.props.users.map(u => <div key={u.id}>
+                props.users.map(u => <div key={u.id}>
                     <span>
                         <div className={style.ava}>
                             <img src={u.photos.small != null ? u.photos.small : noAva}/>
@@ -58,10 +36,10 @@ class Users extends React.Component {
                         <div>
                             {u.followed
                                 ? <button onClick={() => {
-                                    this.props.unfollow(u.id)
+                                    props.unfollow(u.id)
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    this.props.follow(u.id)
+                                    props.follow(u.id)
                                 }}>Follow</button>}
                         </div>
                     </span>
@@ -82,7 +60,5 @@ class Users extends React.Component {
                 </div>)
             }
         </div>
-    }
+    )
 }
-
-export default Users
